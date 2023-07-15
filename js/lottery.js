3a3e0ce7fdd897796b10eb45af74d81830ebdf57
Charith Fragoso <charith.fragoso@gmail.com>
2023-07-16 00:17:55 +0100

    Attempted to check matching numbers to show scores

--- js/lottery.js
+++ js/lottery.js
@@ -8,6 +8,11 @@ const button_run = document.querySelector("#button_run");
         button_run.addEventListener("click", () => {
             
             pick_numbers();
+            // setTimeout(() => {
+            //     alert("Reloading Page")
+            //     location.reload();
+            // }, 1000);
+            getMatchingBalls(my_numbers, generated_numbers);
 
         });
 
@@ -54,16 +59,55 @@ const create_my_numbers = (num) => {
 
     div_num.appendChild(text_num);
     my_numbers_container.appendChild(div_num);
+
 }
 
 const pick_numbers = () => {
 
-    my_numbers = [];
-    while(my_numbers.length < N_NUMS){
+    generated_numbers = [];
+    while(generated_numbers.length < N_NUMS){
         let num = Math.floor((Math.random() * NUM_MAX) + 1);
-        if(!my_numbers.includes(num)) my_numbers.push(num)
+        if(!generated_numbers.includes(num)) generated_numbers.push(num)
     }
+
+    generated_numbers_container.append(generated_numbers);
     console.log("my numbers : ", my_numbers);
+
+}
+
+function getMatchingBalls(my_numbers, generated_numbers){
+
+    var matches = [];
+
+    for(var i = 0; i < my_numbers.length; i++){
+        var userBall = my_numbers[i];
+        var isMatch = false;
+
+        for(var j = 0; j < generated_numbers.length; j++){
+            if(userBall === generated_numbers[j]){
+                isMatch = true;
+                break;
+            }
+        }
+
+        if (isMatch){
+            matches.push(userBall);
+        }
+    }
+
+    if(matches.length == 3){
+        alert("Congratulations, you got 3 matching numbers, you now have a score of 50");
+    }
+    else if(matches.length == 4){
+        alert("Congratulations, you got 4 matching numbers, you now have a score of 100");
+    }
+    else if(matches.length == 5){
+       alert("Congratulations, you got 5 matching numbers, you now have a score of 200");
+    }
+    else if(matches.length == 6){
+        alert("WOW YOU GOT ALL 6 NUMBERS! YOU HAVE A SCORE OF 600");
+    }
+
 }
 
 
