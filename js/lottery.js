723d20c97233df770c9c4fba2783073eaa338c71
Charith Fragoso <charith.fragoso@gmail.com>
2023-07-15 23:47:50 +0100

    Created number picking logic

--- js/lottery.js
+++ js/lottery.js
@@ -8,7 +8,6 @@ const button_run = document.querySelector("#button_run");
         button_run.addEventListener("click", () => {
             
             pick_numbers();
-            //display_numbers();
 
         });
 
@@ -26,6 +25,37 @@ const create_the_grid = () => {
     console.log(grid_container);
 }
 
+grid_container.addEventListener("mouseover", (ev) => {
+    let id = ev.target.id;
+    console.log(`id: ${id}`);
+});
+
+grid_container.addEventListener("click", (ev) => {
+    let id = ev.target.id;
+    let num = ev.target.textContent;
+    console.log(`id: ${id} -----> picked number ${num}`);
+    console.log("Picked number : ", num);
+
+    
+    if(my_numbers.length < N_NUMS && !my_numbers.includes(num)){
+        my_numbers.push(num)
+        grid_container.childNodes[num - 1].classList.add("grid_cursor");
+        create_my_numbers(num);
+    }    
+});
+
+
+const create_my_numbers = (num) => {
+    let div_num = document.createElement("div");
+        div_num.setAttribute("class", "my_numbers");
+        div_num.setAttribute("id", "my_numbers_" + num);
+
+    let text_num = document.createTextNode(num);
+
+    div_num.appendChild(text_num);
+    my_numbers_container.appendChild(div_num);
+}
+
 const pick_numbers = () => {
 
     my_numbers = [];
@@ -33,20 +63,8 @@ const pick_numbers = () => {
         let num = Math.floor((Math.random() * NUM_MAX) + 1);
         if(!my_numbers.includes(num)) my_numbers.push(num)
     }
-
-    //my_numbers.sort((num_1, num_2) => (num_1 - num_2));
     console.log("my numbers : ", my_numbers);
 }
 
-// const display_numbers = () => {
-
-//     for (let i = 0; i < NUM_MAX; i++){
-//         grid_container.childNodes[i].classList.remove("grid_cursor");
-//     }
-//     for(let num of my_numbers){
-//         grid_container.childNodes[num - 1].classList.add("grid_cursor");  
-//     }
-//     console.log(grid_container.childNodes);
-// }
 
 create_the_grid();
